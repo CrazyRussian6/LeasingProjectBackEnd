94675591c61a21f577f79d9675d6a8f8a383eb26
LUKASJ <lukas.janulis@gmail.com>
2018-03-20 14:09:46 +0200

    Spring boot basic app

---
+++ src/main/java/lt/swedbank/itacademy/ItAkaLeasingSystemBackEnd/ItAkaLeasingSystemBackEndApplication.java
@@ -0,0 +1,12 @@
+package lt.swedbank.itacademy.ItAkaLeasingSystemBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItAkaLeasingSystemBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItAkaLeasingSystemBackEndApplication.class, args);
+	}
+}
