a4def06099c2dad91a8208badfdcba41de887a1d
unknown <matas.graj@gmail.com>
2018-03-20 16:21:53 +0200

    created Leasing and VehicleLeasing calasses

---
+++ src/main/java/lt/swedbank/itacademy/ItAkaLeasingSystemBackEnd/beans/documents/Leasing.java
@@ -0,0 +1,85 @@
+package lt.swedbank.itacademy.ItAkaLeasingSystemBackEnd.beans.documents;
+
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Leasing {
+
+    @NotNull
+    private BigDecimal advancePaymentPercent;
+
+    @NotNull
+    private BigDecimal advancePaymentAmount;
+
+    @NotNull
+    private int leasingPeriod;
+
+    @NotNull
+    private BigDecimal margin;
+
+    @NotNull
+    private BigDecimal contractFee;
+
+    @NotNull
+    private BigDecimal assetPrice;
+
+    @NotNull
+    private Date paymentDate;
+
+    public BigDecimal getAdvancePaymentPercent() {
+        return advancePaymentPercent;
+    }
+
+    public void setAdvancePaymentPercent(BigDecimal advancePaymentPercent) {
+        this.advancePaymentPercent = advancePaymentPercent;
+    }
+
+    public BigDecimal getAdvancePaymentAmount() {
+        return advancePaymentAmount;
+    }
+
+    public void setAdvancePaymentAmount(BigDecimal advancePaymentAmount) {
+        this.advancePaymentAmount = advancePaymentAmount;
+    }
+
+    public int getLeasingPeriod() {
+        return leasingPeriod;
+    }
+
+    public void setLeasingPeriod(int leasingPeriod) {
+        this.leasingPeriod = leasingPeriod;
+    }
+
+    public BigDecimal getMargin() {
+        return margin;
+    }
+
+    public void setMargin(BigDecimal margin) {
+        this.margin = margin;
+    }
+
+    public BigDecimal getContractFee() {
+        return contractFee;
+    }
+
+    public void setContractFee(BigDecimal contractFee) {
+        this.contractFee = contractFee;
+    }
+
+    public BigDecimal getAssetPrice() {
+        return assetPrice;
+    }
+
+    public void setAssetPrice(BigDecimal assetPrice) {
+        this.assetPrice = assetPrice;
+    }
+
+    public Date getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(Date paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+}
---
+++ src/main/java/lt/swedbank/itacademy/ItAkaLeasingSystemBackEnd/beans/documents/VehicleLeasing.java
@@ -0,0 +1,51 @@
+package lt.swedbank.itacademy.ItAkaLeasingSystemBackEnd.beans.documents;
+
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+public class VehicleLeasing extends Leasing {
+
+    @NotNull
+    private String manufactorer;
+
+    @NotNull
+    private String model;
+
+    @NotNull
+    private Date manufactoringDate;
+
+    @NotNull
+    private int enginePower;
+
+    public String getManufactorer() {
+        return manufactorer;
+    }
+
+    public void setManufactorer(String manufactorer) {
+        this.manufactorer = manufactorer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Date getManufactoringDate() {
+        return manufactoringDate;
+    }
+
+    public void setManufactoringDate(Date manufactoringDate) {
+        this.manufactoringDate = manufactoringDate;
+    }
+
+    public int getEnginePower() {
+        return enginePower;
+    }
+
+    public void setEnginePower(int enginePower) {
+        this.enginePower = enginePower;
+    }
+}
